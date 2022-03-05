02e2caa55e9eb2ea037469c298bb710e998fd1aa
Kevin Lockett <chlockett@msn.com>
2022-03-04 18:27:22 -0600

    kll-Completed Ticket 21

--- bangazon_reports/urls.py
+++ bangazon_reports/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import (CompletedOrders, ExpensiveProducts, InexpensiveProducts )
+from .views import (CompletedOrders, ExpensiveProducts, InexpensiveProducts, IncompleteOrders)
 
 urlpatterns = [
+    path('completedorders', CompletedOrders.as_view()),
     path('expensiveproducts', ExpensiveProducts.as_view()),
-    path('inexpensiveproducts', InexpensiveProducts.as_view()),
-    path('completedorders', CompletedOrders.as_view())
+    path('incompleteorders', IncompleteOrders.as_view()),
+    path('inexpensiveproducts', InexpensiveProducts.as_view())
 ]
